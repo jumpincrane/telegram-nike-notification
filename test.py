423f37a4a7feebe79a2df5abe65c767f52887ea4
jumpincrane <michalnt98@gmail.com>
2023-04-20 20:35:48 +0200

    hotfix

--- test.py
+++ test.py
@@ -9,13 +9,13 @@ from datetime import date, datetime
 
 
 # init
-link_m = "https://www.nike.com/pl/t/buty-meskie-air-force-1-07-lKPQ6q/CW2288-111"
-product_id_m = "CW2288-111"
-link_f = "https://www.nike.com/pl/t/buty-air-force-1-07-J29nBv/DD8959-100"
-product_id_f = "DD8959-100"
+link_m = "https://www.nike.com/pl/t/buty-meskie-dunk-low-retro-dSsNxQ/DD1391-100"
+product_id_m = "DD1391-100"
+link_f = "https://www.nike.com/pl/t/buty-meskie-dunk-low-retro-dSsNxQ/DD1391-100"
+product_id_f = "DD1391-100"
 
 product_size = "43"
-sleep_time = 5 # in min
+sleep_time = 3 # in min
 
 
 def get_shoe_status(link, product_id, product_size):
@@ -26,7 +26,10 @@ def get_shoe_status(link, product_id, product_size):
         telegram_send.send(messages=[f'{now} \n Something went wrong with request'], silent=False)
         
     soup = BeautifulSoup(html_data, "html.parser")
-    data = json.loads(soup.find('script',text=re.compile('INITIAL_REDUX_STATE')).text.replace('window.INITIAL_REDUX_STATE=','')[0:-1])
+
+    # data = json.loads(soup.find('script',text=re.compile('INITIAL_REDUX_STATE')).text.replace('window.INITIAL_REDUX_STATE=','')[0:-1])
+    temp = json.loads(soup.find('script', type="application/json").text.replace('window.INITIAL_REDUX_STATE=',''))
+    data = temp['props']['pageProps']['initialState']
 
     # getting availables
     ava = data['Threads']['products'][product_id]['availableSkus']
@@ -48,7 +51,7 @@ def get_shoe_status(link, product_id, product_size):
 shoes_status_m = get_shoe_status(link_m, product_id_m, product_size)
 now = datetime.now().strftime("%H:%M:%S")
 if shoes_status_m == True:
-    telegram_send.send(messages=[f'{now} \n First run \n Meskie Air Force One {product_size} jest już dostępne na stronie'], silent=False)
+    telegram_send.send(messages=[f'{now} \n First run \n Meskie Low Dunk Retro {product_size} jest już dostępne na stronie'], silent=False)
 else:
     telegram_send.send(messages=[f'{now} \n First run'], silent=False)
 time.sleep(30)
@@ -61,9 +64,9 @@ while True:
     
 
     if shoes_status_m == True:
-        telegram_send.send(messages=[f'{now} \n Meskie Air Force One {product_size} jest już dostępne na stronie'], silent=False)
+        telegram_send.send(messages=[f'{now} \n Meskie Low Dunk Retro {product_size} jest już dostępne na stronie'], silent=False)
     elif shoes_status_f == True:
-        telegram_send.send(messages=[f'{now} \n Damskie Air Force One {product_size} jest już dostępne na stronie'], silent=False)
+        telegram_send.send(messages=[f'{now} \n Damskie Low Dunk Retro {product_size} jest już dostępne na stronie'], silent=False)
 
     time.sleep(sleep_time * 60)
 
